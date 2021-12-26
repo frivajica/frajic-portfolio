4ec8c55367b9ade67596f43bd49c079e52c0d448
frivajica <fraivan_1@hotmail.com>
2021-12-26 14:56:32 -0600

    Changed stylesheet`s folder structure

---
+++ src/components/PresentationInfo.jsx
@@ -0,0 +1,7 @@
+export const PresentationInfo = () => {
+  return (
+		<div id="presentation-info" >
+				<h1>Francisco Jiménez</h1>
+		</div>
+  );
+};
--- src/layouts/Presentation.jsx
+++ src/layouts/Presentation.jsx
@@ -1,11 +1,10 @@
 import waveTransition from '../assets/transition.svg';
+import { PresentationInfo } from '../components/PresentationInfo';
 
 export const Presentation = () => {
 	return(
 		<div id='presentation'>
-			<div className='info'>
-
-			</div>
+			<PresentationInfo />
 			<div className='transition'>
 				<img src={waveTransition} alt="trasition" />
 			</div>
