bab1ca3148d93da319639a2b7cc50c431ecab20a
frivajica <fraivan_1@hotmail.com>
2021-12-26 16:44:36 -0600

    Stated working with presentation section (WIP)

--- src/components/PresentationInfo.jsx
+++ src/components/PresentationInfo.jsx
@@ -2,6 +2,13 @@ export const PresentationInfo = () => {
   return (
 		<div id="presentation-info" >
 				<h1>Francisco Jiménez</h1>
+				<h3>Frontend Developer</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius enim adipisci dolor laudantium veniam recusandae. Iusto nam atque impedit vel dignissimos voluptatum error blanditiis dicta. Fugit quo neque aperiam aspernatur.
+				</p>
+				<button id='download-resume-btn'>
+					Download Resume
+				</button>
 		</div>
   );
 };
