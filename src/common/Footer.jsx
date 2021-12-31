ec0c9580b88daa92e4497206ea8f83099d42370e
frivajica <fraivan_1@hotmail.com>
2021-12-31 14:01:35 -0600

    Started footer customization

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,7 +1,23 @@
+import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
+
 export const Footer = () => {
 	return(
-		<>
-			<h2>FOOTER</h2>
-		</>
+		<div id="footer">
+			<div id="residence">
+				<h2>Residence</h2>
+				<p>Merida, Yucatán, México</p>
+			</div>
+			<div id='social-networks'>
+				<h2>Social Networks</h2>
+				<div>
+					<FaInstagram />
+					<FaGithub />
+					<FaLinkedinIn />
+				</div>
+			</div>
+			<div id='copyright'>
+				frajic.com © 2022
+			</div>
+		</div>
 	);
 };
