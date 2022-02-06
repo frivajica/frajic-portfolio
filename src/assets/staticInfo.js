295574e09bbf06914bfbf0c10ef0a9ed36cfe961
Fran <fraivan_1@hotmail.com>
2022-02-05 19:27:51 -0600

    added some static information and made it easy to edit.

---
+++ src/assets/staticInfo.js
@@ -0,0 +1,32 @@
+export const staticInfo = {
+	position: "Software Engineer",
+	greeting: "Hi! I'm Francisco and I'm a Full Stack Developer.",
+	presentation: [
+		{
+			paragraph: 1, 
+			text: "Discovering more efficient ways to do things, building solutions and giving everything an aesthetic look are some of my favourite parts of the job.",
+		}, 
+		{
+			paragraph: 2, 
+			text: "I specialize in ReactJs and NodeJs and love working on both sides. Also, given my Data Analysis background, I have strong understanding of relational databases and solid experience on SQL querying.",
+		},
+		{
+			paragraph: 3, 
+			text: "",
+		},
+	],
+	meetFran: [
+		{
+			paragraph: 1, 
+			text: "I discovered my joy for programming after working as a Data Analyst in the food industry."
+		}, 
+		{
+			paragraph: 2, 
+			text: "One of the most important side effects of being curious is that you end up learning something, even if you're not even trying, so I enjoy it as my natural way to be."
+		},
+		{
+			paragraph: 3, 
+			text: "At this moment I'm also interested on Ruby on Rails and Python.", 
+		},
+	],
+};
--- src/components/IntroductionInfo.jsx
+++ src/components/IntroductionInfo.jsx
@@ -1,14 +1,12 @@
+import { staticInfo } from "../assets/staticInfo";
+
 export const IntroductionInfo = () => {
   return (
     <div id="introduction-info">
       <h1>Francisco Jiménez</h1>
-      <h3>Full Stack Developer</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius enim
-        adipisci dolor laudantium veniam recusandae. Iusto nam atque impedit vel
-        dignissimos voluptatum error blanditiis dicta. Fugit quo neque aperiam
-        aspernatur.
-      </p>
+      <h3>{staticInfo.position}</h3>
+      <p>{staticInfo.greeting}</p>
+      {staticInfo.presentation.map( e => <p key={e.paragraph}>{e.text}</p> )}
       <button id="download-resume-btn">Download Resume</button>
     </div>
   );
