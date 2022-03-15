76817349275d0f0e253f685895ea5d620f2f6d1d
Fran <fraivan_1@hotmail.com>
2022-03-14 21:16:09 -0600

    Adding firebase hosting

---
+++ src/Services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_TglIL65upS2hw6Qi8tPJ7XclgjrU6xk",
+  authDomain: "frajic-portfolio.firebaseapp.com",
+  projectId: "frajic-portfolio",
+  storageBucket: "frajic-portfolio.appspot.com",
+  messagingSenderId: "688013452961",
+  appId: "1:688013452961:web:a07bd4a06724f119ba72e0",
+  measurementId: "G-37ECMFQFET"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
