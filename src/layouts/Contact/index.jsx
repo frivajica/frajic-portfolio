c888350771131bd869c3dd4f8c54ce6a971d3af3
Fran <fraivan_1@hotmail.com>
2022-03-18 02:32:05 -0600

    App ready for firebase deploy with github integration.

--- src/layouts/Contact/index.jsx
+++
@@ -1,7 +0,0 @@
-export const Contact = () => {
-	return(
-		<div id='contact'>
-			<h2>Contact me</h2>
-		</div>
-	);
-};
--- src/layouts/Landing/index.jsx
+++ src/layouts/Landing/index.jsx
@@ -1,7 +1,6 @@
 import { Introduction } from 'src/layouts/Introduction'
 import { About } from 'src/layouts/About'
 import { Projects } from 'src/layouts/Projects';
-import { Contact } from 'src/layouts/Contact';
 
 export const Landing = () => {
 	return (
