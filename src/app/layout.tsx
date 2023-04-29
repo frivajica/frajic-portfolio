f6e7a225e0339d1723ef9aea12f2f3e15af2db11
Fran <franivan.j.c@gmail.com>
2023-04-29 00:10:34 -0600

    Adding metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,12 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  applicationName: 'Frajic Portfolio',
   title: "Frajic",
+  keywords: ['Next.js', 'React', 'JavaScript'],
   description: "Personal web site",
+  manifest: '/manifest.json',
+  themeColor: 'black',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
